5059c8263f1067958538bdf6497f5def5d68e2b0
Danilo Nascimento <daconnas.dcn@gmail.com>
2022-01-27 18:43:05 -0300

    add student model

--- apps/school/models.py
+++ apps/school/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+
+class Student(models.Model):
+    name = models.CharField(max_length=40)
+    rg = models.CharField(max_length=9)
+    cpf = models.CharField(max_length=11)
+    birth_date = models.DateField()
+    create_at = models.DateTimeField(editable=False, default=timezone.now)
