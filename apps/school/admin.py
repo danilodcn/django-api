574b762b9c3cd79f775fce286fdd963560f35b3a
Danilo Nascimento <daconnas.dcn@gmail.com>
2022-01-28 12:28:34 -0300

    customize admin site

--- apps/school/admin.py
+++ apps/school/admin.py
@@ -3,5 +3,19 @@ from django.contrib import admin
 from apps.school.models import Course, Student
 
 
-admin.site.register(Student)
-admin.site.register(Course)
+class StudentAdmin(admin.ModelAdmin):
+    list_display = ["id", "name", "cpf", "rg", "birth_date"]
+    list_display_links = ["id", "name"]
+    list_per_page = 20
+    search_fields = ["id", "name", "cpf"]
+
+
+class CourseAdmin(admin.ModelAdmin):
+    list_display = ["id", "title", "code", "level"]
+    list_display_links = ["id", "title"]
+    list_per_page = 20
+    search_fields = ["id", "title", "code", "level"]
+
+
+admin.site.register(Student, StudentAdmin)
+admin.site.register(Course, CourseAdmin)
