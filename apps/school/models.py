57cf1f38b15ff14f54746eb6d96a33c2bbc528e3
Danilo Nascimento <daconnas.dcn@gmail.com>
2022-01-28 11:54:51 -0300

    try using locale

--- apps/school/models.py
+++ apps/school/models.py
@@ -4,15 +4,15 @@ from django.utils.translation import gettext_lazy as _
 
 
 class Student(models.Model):
-    name = models.CharField(max_length=40, blank=False, null=False)
+    name = models.CharField(_("name"), max_length=40, blank=False, null=False)
     rg = models.CharField(max_length=9, blank=False, null=False)
     cpf = models.CharField(max_length=11, blank=False, null=False)
-    birth_date = models.DateField(blank=False, null=False)
+    birth_date = models.DateField(_("birth date"), blank=False, null=False)
     create_at = models.DateTimeField(editable=False, default=timezone.now)
 
     class Meta:
-        verbose_name = "Estudante"
-        verbose_name_plural = "Estudantes"
+        verbose_name = _("Student")
+        verbose_name_plural = _("Students")
 
     def __str__(self) -> str:
         return "{}".format(self.name)
@@ -33,7 +33,7 @@ class Course(models.Model):
     title = models.CharField(max_length=50, blank=False, null=False)
     description = models.TextField(max_length=250, null=True, blank=True)
     level = models.CharField(
-        choices=LEVEL_CHOICES, max_length=1, blank=False, null=False
+        choices=LEVEL_CHOICES, max_length=1, default=BASIC, blank=False, null=False
     )
 
     class Meta:
